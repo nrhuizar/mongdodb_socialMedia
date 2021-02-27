c4623b41420fa6fecb9e671009db2970f6639fd8
nrhuizar <nrhuizar@gmail.com>
2021-02-27 15:59:07 -0800

    updated models

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,19 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ReplySchema = new Schema(
+const ReactionSchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment _id
-    replyId: {
+    // set custom id to avoid confusion with parent thought_id
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
-    replyBody: {
+    reactionBody: {
       type: String,
-      required: true
+      required: true,
+      maxlength: 280
     },
-    writtenBy: {
+    username: {
       type: String,
       required: true,
       trim: true
@@ -30,23 +31,26 @@ const ReplySchema = new Schema(
   }
 );
 
-const CommentSchema = new Schema(
+const ThoughtSchema = new Schema(
   {
-    writtenBy: {
+    thoughtText : {
       type: String,
-      required: true
-    },
-    commentBody: {
-      type: String,
-      required: true
+      required: true,
+      minlength: 1,
+      maxlength: 280
     },
     createdAt: {
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     },
-    // use ReplySchema to validate data for a reply
-    replies: [ReplySchema]
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReactionSchema to validate data for a reply
+    reactions: [ReactionSchema]
   },
   {
     toJSON: {
@@ -57,10 +61,10 @@ const CommentSchema = new Schema(
   }
 );
 
-CommentSchema.virtual('replyCount').get(function() {
+ThoughtSchema.virtual('reactionCount').get(function() {
   return this.replies.length;
 });
 
-const Comment = model('Comment', CommentSchema);
+const Thought = model('Thought', ThoughtSchema);
 
-module.exports = Comment;
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.reduce(
+    (total) => total + 1,
+    0
+  );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
